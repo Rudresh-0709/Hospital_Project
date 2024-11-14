625a47030f52f9498d598aa8857d83b86825b05b
pynuz <anujvmehta99@gmail.com>
2024-11-15 03:05:11 +0530

    connection template made

---
+++ connection.php
@@ -0,0 +1,14 @@
+<!-- Template php file for setting up connection with hospital database -->
+ <!-- used in all php files -->
+<?php 
+    // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT)       // -- helps catch all possible error
+    $servername = "localhost";
+    $username = "root";
+    $password = "maria";
+    $dbname = "hospital";
+
+    // create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // mysqli is the MySQL Improved extension, which provides an interface for interacting with MySQL databases in PHP.
+    // connecting to the database, and performing other database operations.
+?>
