155d992835f679db44e9c1fac1134d39ef6087e1
pynuz <anujvmehta99@gmail.com>
2024-11-15 03:08:42 +0530

    code fetching patient_id from name

--- v1.php
+++ v1.php
@@ -5,36 +5,35 @@
   Thus it helps to not terminate the code and just display the error-->
 
   <?php
-
-    // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT)       // -- helps catch all possible error
-    $servername = "localhost";
-    $username = "root";
-    $password = "maria";
-    $dbname = "hospital";
-
-    // create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // mysqli is the MySQL Improved extension, which provides an interface for interacting with MySQL databases in PHP.
-    // connecting to the database, and performing other database operations.
-
+    require('connection.php');
     if($conn->connect_error){ 
         die("connection Failed : " . $conn->connect_error);
     }
     else{
-        $pid = $_POST['patient_id'];
+        $pname = $_POST['patient_name'];
     }
 
     try{
-    $stmt = $conn->prepare("SELECT admit_id as aid from admit where patient_id = ?");
+    //code fetching patient_id from name
+    $query = $conn->prepare("select patient_id from patient where name = ?");
+    $query->bind_param("s", $pname);
+    $query->execute();
+    // $pid = $query->get_result();         wrong to get actual value
+    $temp = $query->get_result();       // this will give resultSet of type mysqli object
+    $temp = $temp->fetch_assoc();       // even if only one expected output...the resultset is one entire ROW...
+                                        //thus need to get that row as an assoc array of php ['patient_id' => <value>].
+    $pid = $temp['patient_id'];         // thus, need to access the value of patient_id from array.
+
+
+    $stmt = $conn->prepare("SELECT admit_id as aid from admit where patient_id = ? and status = 1");
     // prepare statement used to insert the form data into the user_details table
 
     $stmt->bind_param("i", $pid);
     //  This ensures that the data is properly escaped, preventing SQL injection attacks.
     // "ssss" indicates all parameters are strings.
     $stmt->execute();
-    $result = $stmt->get_result()->fetch_assoc();
+    $result = $stmt->get_result()->fetch_assoc();   //fetch_assoc is used to fetch EACH row as an associative array eg. each row will be like["aid":xxx]
     echo "<span id=\"admit_id\">" .$result['aid']. "</span>";
-
 }
 catch(mysqli_sql_exception $e){
     if($e->getCode() == 1062){
@@ -47,6 +46,8 @@ catch(mysqli_sql_exception $e){
     
 }
 
+// code fetching badge_id from admit_id
+// shouldve gone for another file
 try{
     $stmt = $conn->prepare("SELECT badge_id as bid from badges where admit_id = ?");
     // prepare statement used to insert the form data into the user_details table
@@ -59,7 +60,6 @@ try{
     while ($row = $result2->fetch_assoc()) {
         echo "<option value=\"".$row['bid']."\">".$row['bid']."</option>";
     }
-
 
     $stmt->close();
     $conn->close();
